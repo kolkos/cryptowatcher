f79a4a9343404de38cff88fd059157d92a2f995b
Kolkos <antonkolk@gmail.com>
2019-06-08 14:38:30 +0200

    first commit

---
+++ src/main/java/nl/kolkos/cryptowatcher/CryptowatcherApplication.java
@@ -0,0 +1,13 @@
+package nl.kolkos.cryptowatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptowatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptowatcherApplication.class, args);
+	}
+
+}
